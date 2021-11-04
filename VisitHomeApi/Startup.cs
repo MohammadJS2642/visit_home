463bee7d1a262261459ce492e75c7e9a3f4f7ae7
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-11-04 22:23:49 +0330

    mysqConnector MysqlConnection and EF.Core added conectionstring added

--- VisitHomeApi/Startup.cs
+++ VisitHomeApi/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MySqlConnector;
 
 namespace VisitHomeApi
 {
@@ -32,6 +33,8 @@ namespace VisitHomeApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "VisitHomeApi", Version = "v1" });
             });
+
+            services.AddTransient<MySqlConnection>(_ => new MySqlConnection(Configuration["VisitHome"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
