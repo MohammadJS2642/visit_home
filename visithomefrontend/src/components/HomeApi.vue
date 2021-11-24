fad3faa865b81d09fdbfdcfb686b60896754fa2e
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-11-24 20:57:20 +0330

    change component size for showing better

--- visithomefrontend/src/components/HomeApi.vue
+++ visithomefrontend/src/components/HomeApi.vue
@@ -2,27 +2,27 @@
   <!-- container -->
   <div id="home" class="">
     <!-- <div class="row"> -->
-    <div class="col-12 col-md-4 mb-4">
+    <div class="col-12 col-md-12 mb-4">
       <div class="card h-100">
         <a href="shop-single.html">
-            <!-- src="@/assets/img/feature_prod_01.jpg" -->
+          <!-- src="@/assets/img/feature_prod_01.jpg" -->
           <img
             class="card-img-top"
+            src="/assets/img/feature_prod_01.jpg"
             alt="..."
           />
         </a>
         <div class="card-body">
           <ul class="list-unstyled d-flex justify-content-between">
-            <li class="text-muted text-right">تومان {{price}}</li>
+            <li class="text-muted text-right">تومان {{ price }}</li>
           </ul>
-          <a href="shop-single.html" class="h2 text-decoration-none text-dark"
-            >Gym Weight</a
+          <a
+            href="shop-single.html"
+            class="h2 text-decoration-none text-dark"
+            >{{ title }}</a
           >
-          <p class="card-text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in
-            culpa qui officia deserunt.
-          </p>
-          <p class="text-muted">Reviews (24)</p>
+          <p class="card-text"></p>
+          <!-- <p class="text-muted">Reviews (24)</p> -->
         </div>
       </div>
     </div>
@@ -43,6 +43,7 @@ export default defineComponent({
   data() {
     return {
       homeData: [],
+      tst: "feature_prod_01.jpg",
     };
   },
   props: ["homeid", "title", "price", "picture"],
